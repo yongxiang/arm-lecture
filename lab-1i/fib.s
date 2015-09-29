76b22404acb2568be03590a440f2c9db32559a24
yongxiang <p19920310@gmail.com>
2015-09-29 15:52:13 +0800

    Implement the baseline requirement of hw1
    Implement the fib.s in assembly of each version, recursive and iterative version

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,21 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r4, lr}
+	mov r2, #0
+	mov r1, #1	
+	mov r4, r0
+.Loop:
+	subs r4, r4, #1
+	ble .L4
+	add r0, r1, r2
+	mov r2, r1
+	mov r1, r0
+	b .Loop
+.L4:	
+	pop {r4, pc}		@EPILOG
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	
 
 	.size fibonacci, .-fibonacci
 	.end
